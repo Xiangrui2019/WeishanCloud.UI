9a59ba58edf4fb3a246fa338d9527f349cff33c3
Xiangrui2019 <xiangrui@aiursoft.com>
2019-12-09 12:27:53 +0800

    add server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const fastify = require("fastify");
+const fastify_static = require("fastify-static");
+
+let app = fastify();
+
+app.use((req, resp, next) => {
+    resp.setHeader("Cache-Control", "public, max-age=604800");
+    resp.setHeader("Access-Control-Allow-Origin", "*");
+
+    next();
+});
+
+app.register(fastify_static, {
+    root: __dirname,
+    prefix: "/",
+});
+
+app.listen(80, "0.0.0.0", () => {
+    console.log("[WeishanUI] Server started on 0.0.0.0:80");
+});
